43ae3550b7a0d0e5fcf7ee4e603a48330a4eecca
yorah <yorahamim@gmail.com>
2026-04-05 16:19:31 +0300

    try1 usermangement

---
+++ TimeToSchool/Adapters/UserViewHolder.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.RecyclerView.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TimeToSchool.Adapters
+{
+    public class UserViewHolder : RecyclerView.ViewHolder
+    {
+        public TextView FirstName, LastName;
+        public ImageView IvAvatar;
+        public UserViewHolder(View itemView, Action<int> listener) : base(itemView)
+        {
+            FirstName = itemView.FindViewById<TextView>(Resource.Id.tvFirstName);
+            LastName = itemView.FindViewById<TextView>(Resource.Id.tvLastName);
+            //IvAvatar = itemView.FindViewById<ImageView>(Resource.Id.ivAvatar);
+
+            itemView.Click += (sender, e) => listener(base.LayoutPosition);
+        }
+    }
+}
---
+++ TimeToSchool/Adapters/UsersRViewAdapter .cs
@@ -0,0 +1,58 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.RecyclerView.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TimeToSchool.BusinessLogic;
+using TimeToSchool.Model;
+using TimeToSchool.Service;
+
+namespace TimeToSchool.Adapters
+{
+    public class UsersRViewAdapter : RecyclerView.Adapter
+    {
+
+        Context context;
+        List<User> users;
+        public event EventHandler<int> ItemClick;
+
+        public UsersRViewAdapter(Context context, List<User> users)
+        {
+            this.context = context;
+            this.users = users;
+        }
+
+        public override int ItemCount => users.Count;
+
+        void OnClick(int position)
+        {
+            if (ItemClick != null)
+                ItemClick(this, position);
+        }
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            if (holder is UserViewHolder userViewHolder)
+            {
+                userViewHolder.FirstName.Text = users[position].FirstName;
+                userViewHolder.LastName.Text = users[position].LastName;
+                userViewHolder.IvAvatar.SetImageResource(users[position].ImageId);
+            }
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            LinearLayout layout = (LinearLayout)LayoutInflater.From(context)
+                .Inflate(Resource.Layout.user_item, parent, false);
+
+            UserViewHolder viewHolder = new UserViewHolder(layout, OnClick);
+            return viewHolder;
+        }
+    }
+}
